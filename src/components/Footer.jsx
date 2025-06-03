b4979203846401edd084324ea970ed21db01fe4e
Hasan Alhennawi <71322842+Hasan-Alhennawi@users.noreply.github.com>
2025-06-03 11:42:58 +0200

    Delete src/components/Footer.jsx

--- src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer() {
-  return (
-    <footer className="text-center py-8 text-sm text-gray-500">
-      &copy; 2025 Mercenary Game | Token: HSN
-    </footer>
-  );
-}
-export default Footer;
